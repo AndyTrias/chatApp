b3133cea9016bc861bf761cb13213ba09d8974e3
AndyTrias <andrestrias03@gmail.com>
2022-09-12 20:11:01 -0300

    input error

--- chatapp/events.py
+++ chatapp/events.py
@@ -1,12 +1,11 @@
 from . import socketio
 from flask_socketio import emit
-from flask import request
 
 
 @socketio.on('chatMessage')
 def handle_message(msg):
     print('received message: ' + msg)
-    emit("message", msg, to=request.sid)
+    emit("message", msg, broadcast=True)
     # TODO - Save message to database
     # TODO - Send message to all users
     pass
