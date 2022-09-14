29b81de1aa35baa54b6af54e0d1e3032de82d32a
AndyTrias <andrestrias03@gmail.com>
2022-09-14 15:39:19 -0300

    send and receive OK

--- chatapp/events.py
+++ chatapp/events.py
@@ -5,7 +5,7 @@ from flask_socketio import emit
 @socketio.on('chatMessage')
 def handle_message(msg):
     print('received message: ' + msg)
-    emit("message", msg, broadcast=True)
+    emit("message", msg, broadcast=True, include_self=False)
     # TODO - Save message to database
     # TODO - Send message to all users
     pass
--- chatapp/static/socket.js
+++ chatapp/static/socket.js
@@ -9,52 +9,32 @@ document.addEventListener("DOMContentLoaded", function (){
     let input = document.querySelector("#message");
 
     document.querySelector("#sendmsg").addEventListener("click", function(){
+        addMessage(input.value, "start");
+
+        // Clear input and send message
         socket.emit("chatMessage", input.value);
         input.value = ""
     })
 
     socket.addEventListener("message", function (msg) {
-        console.log(msg);
+        addMessage(msg, "end");
+    });
+});
+
+ function addMessage(msg, position) {
         let mainDiv = document.querySelector("#chats");
+
+        // First Div
         let d = document.createElement("div");
+        d.className = `d-flex flex-row justify-content-${position}`;
+
+        // Paragraph
         let p = document.createElement("p");
         p.innerHTML = msg;
         p.className = "small p-2 ms-3 mb-1 rounded-3"
         p.style.backgroundColor = "#f5f6f7"
+
+        // Append
         d.appendChild(p);
         mainDiv.appendChild(d);
-
-
-        // Ad alternative to the above code is to use the following code:
-
-        // <div className="d-flex flex-row justify-content-start">
-        //     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
-        //          alt="avatar 1" style="width: 45px; height: 100%;">
-        //         <div>
-        //             <p className="small p-2 ms-3 mb-1 rounded-3" style="background-color: #f5f6f7;">how are you?</p>
-        //             {#                            <p class="small p-2 ms-3 mb-1 rounded-3" style="background-color: #f5f6f7;">{{message.content}}</p>#}
-        //             {#                            <p class="small ms-3 mb-3 rounded-3 text-muted float-end">{{message.sentAt}}</p>#}
-        //         </div>
-        // </div>
-
-    });
-
-})
-
-
-
-
-/* $(document).ready(function(){
-    $('.contact').on('click',function(){
-     var layout = $(this).data('contacto');
-     console.log(layout);
-      $.ajax({
-       url: "/chat",
-       type: "get",
-       data: {layout: layout},
-       success: function(response) {
-         var new_html = response.html;
-       },
-      });     
-   });
- }); */
+    }
